ab6941fc412cf6b9624b486f69d9c1df8dd4a55a
takasicode <infofadhil29@gmail.com>
2023-02-23 09:30:30 +0700

    shoecare web basic

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function () {
+  var nav = document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  var menuToggle = document.querySelector(".toggle");
+  var menu = document.querySelector(".menu");
+
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
